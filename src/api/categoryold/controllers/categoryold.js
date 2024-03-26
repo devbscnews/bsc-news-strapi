669b420392ec62f59965e115a3b49506eefe8bef
sammohdnak <sammohdnak@gmail.com>
2024-03-26 23:34:42 +0400

    Removed Unwanted Tables

--- src/api/categoryold/controllers/categoryold.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- *  categoryold controller
- */
-
-const { createCoreController } = require("@strapi/strapi").factories;
-
-module.exports = createCoreController("api::categoryold.categoryold");
--- src/api/categoryold/services/categoryold.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- * categoryold service.
- */
-
-const { createCoreService } = require("@strapi/strapi").factories;
-
-module.exports = createCoreService("api::categoryold.categoryold");
