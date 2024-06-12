9728039471fda1758faa58c5003f24c6aedbeff4
sammohdnak <sammohdnak@gmail.com>
2024-06-13 00:51:38 +0530

    Automatic Cache Purge And Cloudflare ENV

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,32 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://storage.bsc.news", // added this line
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            // `https://${env("AWS_BUCKET")}.s3.${env(
+            //   "AWS_REGION"
+            // )}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
---
+++ src/api/post/content-types/post/lifecycles.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const cachePurge = async (slugname) => {
+  const fe_url = process.env.FRONT_END_URL;
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  try {
+    let res1 = await fetch(`${fe_url}/api/revalidate?tag=all-routes`);
+    let res1Data = await res1.json();
+    await delay(5000);
+    let res2 = await fetch(`${fe_url}/api/revalidate?tag=all-posts`);
+    let res2Data = await res2.json();
+    await delay(5000);
+    if (slugname) {
+      let res3 = await fetch(`${fe_url}/api/revalidate?tag=${slugname}`);
+      let res3Data = await res3.json();
+    }
+  } catch (error) {}
+};
+
+module.exports = {
+  afterCreate(event) {
+    const { result, params } = event;
+    cachePurge(result.Slug);
+    console.log("Created", result.Slug);
+  },
+  afterUpdate(event) {
+    const { result, params } = event;
+    cachePurge(result.Slug);
+    console.log("Updated", result.Slug);
+  },
+  afterDelete(event) {
+    const { result, params } = event;
+    cachePurge(result.Slug);
+    console.log("Deleted", result.Slug);
+  },
+};
