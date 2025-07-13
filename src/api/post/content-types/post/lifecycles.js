794b348be3bd0cfc960d1a24e97befe1dd179026
sammohdnak <sammohdnak@gmail.com>
2025-07-13 23:08:52 +0530

    New Purge Mechanism

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -24,20 +24,44 @@ const cachePurge = async (slugname) => {
   } catch (error) {}
 };
 
+const cachePurgeV2 = async (data) => {
+  const fe_url = process.env.FRONT_END_URL;
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  try {
+    await delay(2000);
+
+    let res3 = await fetch(`${fe_url}/api/purgecache`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    let res3Data = await res3.json();
+    console.log(res3Data.data.cfPurgeData);
+
+    console.log("Successfully Purged");
+  } catch (error) {}
+};
+
 module.exports = {
   afterCreate(event) {
     const { result, params } = event;
-    cachePurge(result.Slug);
+
+    // cachePurge(result.Slug);
+    cachePurgeV2(result);
     console.log("Created", result.Slug);
   },
   afterUpdate(event) {
     const { result, params } = event;
-    cachePurge(result.Slug);
+
+    // cachePurge(result.Slug);
+    cachePurgeV2(result);
     console.log("Updated", result.Slug);
   },
   afterDelete(event) {
     const { result, params } = event;
+
     cachePurge(result.Slug);
+    cachePurgeV2(result);
     console.log("Deleted", result.Slug);
   },
 };
